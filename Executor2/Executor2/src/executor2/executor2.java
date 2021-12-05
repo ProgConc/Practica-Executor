65d1edf099eacb9f15e8c4795a250f198b44a103
ProgConc <93448957+ProgConc@users.noreply.github.com>
2021-12-05 20:13:43 +0100

    Executor2

---
+++ Executor2/Executor2/src/executor2/executor2.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package executor2;
+
+/**
+ *
+ * @author A
+ */
+
+import java.time.Duration;
+
+import java.time.Instant;
+
+import java.util.concurrent.ExecutorService;
+
+import java.util.concurrent.Executors;
+
+import java.util.concurrent.TimeUnit;
+
+
+
+public class executor2 {
+	private static final Instant INICIO = Instant.now(); 
+	public static void main(String[] args) {
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+                Runnable tarea= ()->{
+			Log("Inicio de la tarea");
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			Log("Finaliza la tarea");
+		};
+		executor.submit(tarea);
+		executor.shutdown();
+	}
+
+	
+
+	private static void Log(Object mensaje) {
+		System.out.println(String.format("%s [%s] %s", 
+			Duration.between(INICIO, Instant.now()), Thread.currentThread().getName(), mensaje.toString()));
+	}
+}
